4d8cc05f688a9b1d4759cfd5628ff7c582bbd62f
hscottvo <56327086+hscottvo@users.noreply.github.com>
2023-12-22 12:38:54 -0800

    Chapter 8

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,65 @@
-use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
-
-
+use std::collections::HashMap;
+fn is_vowel(c: char) -> bool {
+    c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+}
 fn main() {
-    println!("Guess the number!");
+    let mut a = "hello".to_string();
+    a.push_str("hello_2");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let b = String::from("hi_3");
 
-    println!("temp: Current random number is: {secret_number}");
+    println!("Hello, {}", a);
 
-    loop {
-        println!("Enter your number.");
-    
-        let mut guess = String::new();
+    let c = a + &b;
+    println!("{}", c);
 
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+    let mut v = vec![1, 2, 3];
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
+    for i in &v {
+        println!("{}", i);
+    }
 
-        println!("You guessed {guess}");
+    for i in &mut v {
+        *i += 10;
+    }
 
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small"),
-            Ordering::Greater => println!("Too big"),
-            Ordering::Equal => {
-                println!("Correct!");
-                break;
-            },
+    for i in &v {
+        println!("{}", i);
+    }
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    // field_name and field_value are invalid at this point, try using them and
+    // see what compiler error you get!
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
+
+    let pigify = String::from("hi there, my apple is crazy");
+    for i in pigify.split_whitespace() {
+        let mut string_i = String::from(i);
+        let first_char = string_i.remove(0);
+        let mut ret: String = String::from("");
+        if is_vowel(first_char) {
+            ret = String::from(i);
+            ret.push_str("-hay");
+        } else {
+            ret = String::from(string_i);
+            ret.push_str("-");
+            ret.push(first_char);
+            ret.push_str("ay");
         }
+        println!("{0} turns into {1}", i, ret);
     }
 }
